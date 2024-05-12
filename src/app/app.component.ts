5ae8c207a5de9dc6d044b2238cdbdf50253a1fb6
phte1100 <phte1100@student.miun.se>
2024-05-12 22:22:54 +0200

    fixat footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TabsComponent } from './tabs/tabs.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TabsComponent],
+  imports: [RouterOutlet, TabsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
