8a339294e98329d4903a3ff1541cc8e0602c1762
phte1100 <phte1100@student.miun.se>
2024-05-29 11:19:00 +0200

    kommenterat lite kod

--- src/app/services/list.service.ts
+++ src/app/services/list.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CourseData } from '../kurser/kurser.component';
 
+// Dekorerar klassen som en injicerbar tjänst som finns tillgänglig över hela applikationen.
 @Injectable({
     providedIn: 'root',
 })
@@ -11,6 +12,7 @@ export class ListService {
 
     constructor(private http: HttpClient) {}
 
+    // Metod för att hämta kurser från en extern JSON-fil över HTTP, returnerar en Observable av CourseData-arrayer.
     getCourses(): Observable<CourseData[]> {
         return this.http.get<CourseData[]>('https://matdah.github.io/DT208G---Programmering-i-TypeScript/Moment%205%20-%20Projekt/miun_courses.json');
       }
--- src/app/services/schema.service.ts
+++ src/app/services/schema.service.ts
@@ -9,11 +9,13 @@ export class SchemaService {
 
   constructor() {}
 
+  // Metod för att hämta alla kurser från localStorage.
   getCourses(): CourseData[] {
     const storedCourses = localStorage.getItem(this.localStorageKey);
     return storedCourses ? JSON.parse(storedCourses) : [];
   }
 
+  // Metod för att lägga till en kurs i localStorage, om den inte redan finns.
   addCourse(course: CourseData) {
     const courses = this.getCourses();
     if (!this.courseExists(course.courseCode)) {
@@ -22,12 +24,14 @@ export class SchemaService {
     }
   }
 
+ // Metod för att ta bort en kurs från localStorage baserat på kurskod
   removeCourse(courseCode: string) {
     let courses = this.getCourses();
     courses = courses.filter(course => course.courseCode !== courseCode);
     localStorage.setItem(this.localStorageKey, JSON.stringify(courses));
   }
 
+  // Kontrollerar om en kurs redan finns i localStorage
   public courseExists(courseCode: string): boolean {
     const courses = this.getCourses();
     return courses.some(course => course.courseCode === courseCode);
